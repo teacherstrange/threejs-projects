7a863b05d42e3ba605e0128d13023b98704300e9
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-05-01 21:18:36 +0200

    no message

--- frontend/src/containers/StackTowerPage/StackTowerPage.tsx
+++ frontend/src/containers/StackTowerPage/StackTowerPage.tsx
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic';
 
 import { Wrapper } from './styled/Wrapper';
 import { CanvasWrapper } from './styled/CanvasWrapper';
+import { Head } from 'utils/seo/Head';
 
 const GravityObjects = dynamic(
   () => import('./components/StackTower/StackTower'),
@@ -14,6 +15,13 @@ interface Props {}
 export default function StackTowerPage(props: Props) {
   return (
     <>
+      <Head
+        shouldIndex={true}
+        description="Online stack game made with three.js"
+        ogImage={{ url: 'https://threejs-projects.vercel.app/888.jpg' }}
+        ogType="website"
+        title="Stack game"
+      />
       <Wrapper>
         <CanvasWrapper>
           <GravityObjects />
